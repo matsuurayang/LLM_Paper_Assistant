19ac9bed728c7403fb1ca7752a0c6fefed61b3f8
matsuurayang <139259503+matsuurayang@users.noreply.github.com>
2025-02-13 13:04:28 +0800

    Delete paper_parser.py

--- paper_parser.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-
-def extract_text_from_pdf(pdf_path: str) -> str:
-    """从 PDF 文件中提取文本"""
-    with pdfplumber.open(pdf_path) as pdf:
-        text = "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
-    return text
